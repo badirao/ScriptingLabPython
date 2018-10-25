80353b129cede8dbcf8eea1ff838889cc7a94497
Adithya Rao <raoadi.8@gmail.com>
2018-10-26 01:34:39 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -10,10 +10,8 @@ vowels in the string.
 
 
 
-
-
-
-from collections import Counter
+from operator import itemgetter
+dictionary = {}
 class r_string:
 	
 		
@@ -30,31 +28,34 @@ String2 = r_string(raw_input('enter string2'))
 String3 = r_string(raw_input('enter string3'))
 
 s1=String1.reversal()
-c1 = Counter(s1)
-n1 = c1['a'] + c1['e'] + c1['i'] + c1['o'] + c1['u']
+
+
 s2= String2.reversal()
-c2 = Counter(s2)
-n2 = c2['a'] + c2['e'] + c2['i'] + c2['o'] + c2['u']
+
+
 s3= String3.reversal()
-c3 = Counter(s3)
-n3 = c3['a'] + c3['e'] + c3['i'] + c3['o'] + c3['u']
-numberlist = [n1,n2,n3]
-stringlist = [s1,s2,s3]
-nlist =[stringlist,numberlist]
-Max = numberlist.index(max(numberlist))
-Min = numberlist.index(min(numberlist))
 
-FirstString = stringlist[Max]
-LastString = stringlist[Min]
 
-del stringlist[Max]
-del stringlist[Min]
+vowels1 = 0
+vowels2 = 0
+vowels3 = 0
 
-MedString = stringlist[0]
+for i in range(0,len(s1)):
+	if s1[i] == 'a' or s1[i] == 'e' or s1[i] == 'i' or s1[i] == 'o' or s1[i] == 'u':
+		vowels1 = vowels1 + 1
+for i in range(0,len(s2)):
+	if s2[i] == 'a' or s2[i] == 'e' or s2[i] == 'i' or s2[i] == 'o' or s2[i] == 'u':
+		vowels2 = vowels2 + 1  
+for i in range(0,len(s3)):
+	if s3[i] == 'a' or s3[i] == 'e' or s3[i] == 'i' or s3[i] == 'o' or s3[i] == 'u':
+		vowels3 = vowels3 + 1 
 
-print FirstString
-print MedString
-print LastString
+dictionary[s1] = vowels1
+dictionary[s2] = vowels2
+dictionary[s3] = vowels3
+
+sorted_dict = sorted(dictionary.items(),key = itemgetter(1),reverse = True)
+print sorted_dict
 
 
 
